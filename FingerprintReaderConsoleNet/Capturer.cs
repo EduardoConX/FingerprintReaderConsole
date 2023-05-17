af74c6c053db898d29884226b60134061f3656ce
Eduardo Pacheco <eduardopachecoquintanar@gmail.com>
2023-05-17 16:18:39 -0600

    Agregar archivos de proyecto.

---
+++ FingerprintReaderConsoleNet/Capturer.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FingerprintReaderConsoleNet
+{
+    public partial class Capturer : DPFP.Capture.EventHandler
+    {
+        private DPFP.Capture.Capture _capturer;
+        private DPFP.Processing.Enrollment _enroller;
+        protected virtual void Init()
+        {
+            try
+            {
+                _capturer = new DPFP.Capture.Capture();               // Create a capture operation.
+
+                if (null != _capturer)
+                {
+                    _capturer.EventHandler = this;                    // Subscribe for capturing events.
+                    Console.WriteLine("Operación de captura inicializada correctamente");
+                }
+                else
+                {
+                    Console.WriteLine("¡No se pudó inicializar la operación de captura!");
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("¡No se pudó inicializar la operación de captura!");
+                Console.WriteLine(ex.Message);
+            }
+        }
+
+        protected virtual void Process(DPFP.Sample Sample)
+        {
+            // Draw fingerprint sample image.
+            Console.WriteLine("Dibujando");
+        }
+
+        protected void Start()
+        {
+            if (null != _capturer)
+            {
+                try
+                {
+                    _capturer.StartCapture();
+                    Console.WriteLine("Escanea tu huella usando el lector");
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("No se puede iniciar la captura");
+                    Console.WriteLine(ex.Message);
+                }
+            }
+        }
+
+        protected void Stop()
+        {
+            if (null != _capturer)
+            {
+                try
+                {
+                    _capturer.StopCapture();
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("No se puede terminar la captura");
+                    Console.WriteLine(ex.Message);
+                }
+            }
+        }
+
+        #region EventHandler Members:
+
+        public void OnComplete(object Capture, string ReaderSerialNumber, DPFP.Sample Sample)
+        {
+            Console.WriteLine("La muestra de huellas dactilares fue capturada.");
+            Console.WriteLine("Escanea la misma huella dactilar de nuevo.");
+            Process(Sample);
+        }
+
+        public void OnFingerGone(object Capture, string ReaderSerialNumber)
+        {
+            Console.WriteLine("El dedo fue retirado del lector de huellas dactilares..");
+        }
+
+        public void OnFingerTouch(object Capture, string ReaderSerialNumber)
+        {
+            Console.WriteLine("El lector de huellas digitales fue tocado.");
+        }
+
+        public void OnReaderConnect(object Capture, string ReaderSerialNumber)
+        {
+            Console.WriteLine("El lector de huellas digitales fue conectado.");
+        }
+
+        public void OnReaderDisconnect(object Capture, string ReaderSerialNumber)
+        {
+            Console.WriteLine("El lector de huellas digitales estaba desconectado.");
+        }
+
+        public void OnSampleQuality(object Capture, string ReaderSerialNumber, DPFP.Capture.CaptureFeedback CaptureFeedback)
+        {
+            if (CaptureFeedback == DPFP.Capture.CaptureFeedback.Good)
+                Console.WriteLine("La calidad de la muestra de huellas dactilares es buena.");
+            else
+                Console.WriteLine("La calidad de la muestra de huellas dactilares es mala.");
+        }
+        #endregion
+
+        protected DPFP.FeatureSet ExtractFeatures(DPFP.Sample Sample, DPFP.Processing.DataPurpose Purpose)
+        {
+            DPFP.Processing.FeatureExtraction Extractor = new DPFP.Processing.FeatureExtraction();  // Create a feature extractor
+            DPFP.Capture.CaptureFeedback feedback = DPFP.Capture.CaptureFeedback.None;
+            DPFP.FeatureSet features = new DPFP.FeatureSet();
+            Extractor.CreateFeatureSet(Sample, Purpose, ref feedback, ref features);            // TODO: return features as a result?
+            if (feedback == DPFP.Capture.CaptureFeedback.Good)
+                return features;
+            else
+                return null;
+        }
+    }
+}
---
+++ FingerprintReaderConsoleNet/Program.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FingerprintReaderConsoleNet
+{
+    internal class Program: Capturer
+    {
+        public delegate void OnTemplateEventHandler(DPFP.Template template);
+        private DPFP.Template Template;
+        private DPFP.Processing.Enrollment Enroller;
+        public string encoded;
+
+        static void Main(string[] args)
+        {
+            Program program = new Program();
+            program.Init();
+        }
+
+        protected override void Init()
+        {
+            base.Init();
+            Enroller = new DPFP.Processing.Enrollment();            // Create an enrollment.
+            UpdateStatus();
+            base.Start();
+            Console.ReadKey();
+        }
+
+        private void OnTemplate(DPFP.Template template)
+        {
+            Template = template;
+            if (Template != null)
+            {
+                Console.WriteLine("La huella fue registrada exitosamente");
+                byte[] streamHuella = Template.Bytes;
+
+                encoded = Convert.ToBase64String(streamHuella);
+                Console.WriteLine(encoded);
+            }
+            else
+            {
+                Console.WriteLine("El registro de huela digital falló");
+            }
+        }
+
+        protected override void Process(DPFP.Sample Sample)
+        {
+            base.Process(Sample);
+
+            // Process the sample and create a feature set for the enrollment purpose.
+            DPFP.FeatureSet features = base.ExtractFeatures(Sample, DPFP.Processing.DataPurpose.Enrollment);
+
+            // Check quality of the sample and add to enroller if it's good
+            if (features != null) try
+                {
+                    Console.WriteLine("Se creó el set de huellas dactilares.");
+                    Enroller.AddFeatures(features);     // Add feature set to template.
+                }
+                finally
+                {
+                    UpdateStatus();
+
+                    // Check if template has been created.
+                    switch (Enroller.TemplateStatus)
+                    {
+                        case DPFP.Processing.Enrollment.Status.Ready:   // report success and stop capturing
+                            OnTemplate(Enroller.Template);            
+                            Stop();
+                            break;
+
+                        case DPFP.Processing.Enrollment.Status.Failed:  // report failure and restart capturing
+                            Enroller.Clear();
+                            Stop();
+                            UpdateStatus();
+                            OnTemplate(null);
+                            base.Start();
+                            break;
+                    }
+                }
+        }
+
+        private void UpdateStatus()
+        {
+            // Show number of samples needed.
+            Console.WriteLine(String.Format("Muestras faltantes: {0}", Enroller.FeaturesNeeded));
+        }
+    }
+}
